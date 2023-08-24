5501383b87623459e0fa9a56bda9429d835cc762
julysNICK <julystube@gmail.com>
2023-08-24 15:04:24 -0300

    add delayed and touch animation

--- lib/main.dart
+++ lib/main.dart
@@ -16,21 +16,231 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const DelayedAnimation(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class DelayedAnimation extends StatefulWidget {
+  const DelayedAnimation({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<DelayedAnimation> createState() => _DelayedAnimationState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+class _DelayedAnimationState extends State<DelayedAnimation>
+    with TickerProviderStateMixin {
+  Animation animation = const AlwaysStoppedAnimation(0.0);
+
+  Animation delayAnimated = const AlwaysStoppedAnimation(0.0);
+
+  Animation muchDelayAnimated = const AlwaysStoppedAnimation(0.0);
+
+  late AnimationController animationController;
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    animation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+      parent: animationController,
+      curve: Curves.fastOutSlowIn,
+    ));
+
+    delayAnimated = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+      parent: animationController,
+      curve: const Interval(0.5, 1.0, curve: Curves.fastOutSlowIn),
+    ));
+
+    muchDelayAnimated = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+      parent: animationController,
+      curve: const Interval(0.8, 1.0, curve: Curves.fastOutSlowIn),
+    ));
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+
+    return AnimatedBuilder(
+        animation: animationController,
+        builder: (BuildContext context, Widget? child) {
+          return Scaffold(
+              appBar: AppBar(
+                title: Text(widget.title),
+              ),
+              body: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  child: Center(
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: [
+                        Transform(
+                          transform: Matrix4.translationValues(
+                              animation.value * width, 0.0, 0.0),
+                          child: Center(
+                            child: Container(
+                              padding: const EdgeInsets.all(10.0),
+                              child: const Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Login',
+                                    style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Transform(
+                          transform: Matrix4.translationValues(
+                              delayAnimated.value * width, 0.0, 0.0),
+                          child: Center(
+                            child: Container(
+                              padding: const EdgeInsets.all(10.0),
+                              child: const Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  TextField(
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: 'Username',
+                                    ),
+                                  ),
+                                  TextField(
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: 'Password',
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Transform(
+                          transform: Matrix4.translationValues(
+                              muchDelayAnimated.value * width, 0.0, 0.0),
+                          child: Center(
+                            child: Container(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      print("Login");
+                                    },
+                                    child: const Text('Login'),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ));
+        });
+  }
+}
+
+class GestureAnimation extends StatefulWidget {
+  const GestureAnimation({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<GestureAnimation> createState() => _GestureAnimationState();
+}
+
+class _GestureAnimationState extends State<GestureAnimation>
+    with TickerProviderStateMixin {
+  Animation animation = const AlwaysStoppedAnimation(0.0);
+  late AnimationController animationController;
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    animation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+      parent: animationController,
+      curve: Curves.fastOutSlowIn,
+    ));
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              print("Tap");
+            },
+            child: Container(
+              height: 70.0,
+              width: 100.0,
+              padding: const EdgeInsets.all(10.0),
+              decoration: const BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              ),
+              child: const Text("Click Me"),
+            ),
+          ),
+        ));
+  }
+}
+
+class LoginAnimationProvider extends StatefulWidget {
+  const LoginAnimationProvider({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<LoginAnimationProvider> createState() => _LoginAnimationProviderState();
+}
+
+class _LoginAnimationProviderState extends State<LoginAnimationProvider>
+    with TickerProviderStateMixin {
   Animation animation = const AlwaysStoppedAnimation(0.0);
   late AnimationController animationController;
   @override
